1c7c6f0180e85d40f259e39bbffc445ec0842564
evgenijaZ <evg.zubrich@gmail.com>
2018-10-27 13:54:58 +0300

    added User class

---
+++ src/main/java/edu/training/task06/src/main/java/edu/training/loginform/model/User.java
@@ -0,0 +1,46 @@
+package edu.training.loginform.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String firstName;
+    private String secondName;
+
+    public User() {
+    }
+
+    public User(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+}
