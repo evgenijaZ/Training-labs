e1f3bf044a09ed048b374f4abcb5625b610b8bc8
evgenijaZ <evg.zubrich@gmail.com>
2018-10-27 13:55:14 +0300

    added repository

---
+++ src/main/java/edu/training/task06/src/main/java/edu/training/loginform/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package edu.training.loginform.repository;
+
+import edu.training.loginform.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAll();
+}
