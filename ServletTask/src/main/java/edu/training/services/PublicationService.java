12beb4d67faaeaa29a91d37f61d0ee6cbb65466f
evgenijaZ <evg.zubrich@gmail.com>
2018-11-06 22:03:12 +0200

    added publication service

---
+++ ServletTask/src/main/java/edu/training/services/PublicationService.java
@@ -0,0 +1,37 @@
+package edu.training.services;
+
+import edu.training.model.dao.ElectronicPublicationDao;
+import edu.training.model.dao.IDao;
+import edu.training.model.dao.PaperPublicationDao;
+import edu.training.model.entities.Publication;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class PublicationService {
+    private IDao<? extends Publication, Long> elPubDAO;
+    private IDao<? extends Publication, Long> papPubDAO;
+
+
+    public PublicationService(DataSource dataSource) {
+        Objects.requireNonNull(dataSource);
+        elPubDAO = new ElectronicPublicationDao(dataSource);
+        papPubDAO = new PaperPublicationDao(dataSource);
+    }
+
+    public List<Publication> getAll() {
+        List<Publication> result = new ArrayList<>();
+        result.addAll(elPubDAO.getAll());
+        result.addAll(papPubDAO.getAll());
+        return result;
+    }
+
+    public Publication findById(long id) {
+        Publication publication = elPubDAO.getByKey(id);
+        if (publication == null) publication = papPubDAO.getByKey(id);
+        return publication;
+    }
+}
