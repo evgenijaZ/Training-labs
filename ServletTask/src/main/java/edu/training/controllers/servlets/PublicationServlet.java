f22a3c1613232e452f113e5c7a2f17464810b581
evgenijaZ <evg.zubrich@gmail.com>
2018-11-08 20:09:55 +0200

    added filtering to servlet

--- ServletTask/src/main/java/edu/training/controllers/servlets/PublicationServlet.java
+++ ServletTask/src/main/java/edu/training/controllers/servlets/PublicationServlet.java
@@ -1,9 +1,9 @@
 package edu.training.controllers.servlets;
 
 import edu.training.model.entities.Publication;
-import edu.training.services.FilteringService;
-import edu.training.services.LibraryService;
-import edu.training.services.PublicationService;
+import edu.training.model.services.FilteringService;
+import edu.training.model.services.PublicationService;
+import edu.training.model.services.ReferencesService;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -23,51 +23,74 @@ public class PublicationServlet extends HttpServlet {
     private void processRequest(
             HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         PublicationService service = new PublicationService(InitContext.getDataSource());
 
         RequestDispatcher dispatcher;
-
         String publicationID = request.getParameter("id");
         if (publicationID != null) {
-
             int id = Integer.parseInt(publicationID);
             Publication publication = service.findById((long) id);
             String showReferences = request.getParameter("references");
             if (showReferences != null && showReferences.equals("true")) {
-                List<Publication> publications = service.getAll();
-                LibraryService libraryService = new LibraryService();
-                request.setAttribute("publications",
-                        Objects.requireNonNull(
-                                libraryService
-                                        .getPublicationsFromReferences(
-                                                publication.getReferences(),
-                                                publications)));
-                dispatcher = request.getRequestDispatcher(SHOW_ALL_PATH);
+                dispatcher = getReferencesDispatcher(request, service, publication);
             } else {
-                request.setAttribute("publication", Objects.requireNonNull(publication));
-                dispatcher = request.getRequestDispatcher(SHOW_ONE_PATH);
+                dispatcher = getPublicationDispatcher(request, publication);
             }
-
-
         } else {
-            List<Publication> publications = service.getAll();
-            String sorted = request.getParameter("sorted");
-            if (sorted != null && sorted.equals("true")) {
-                FilteringService filteringService = new FilteringService();
-                publications = filteringService.sortByRelevance(publications);
-            }
-            request.setAttribute("publications", publications);
-            dispatcher = request.getRequestDispatcher(SHOW_ALL_PATH);
+            dispatcher = getAllPublicationsDispatcher(request, service);
         }
-
         dispatcher.forward(request, response);
+    }
+
+    private RequestDispatcher getPublicationDispatcher(HttpServletRequest request, Publication publication) {
+        RequestDispatcher dispatcher;
+        request.setAttribute("publication", publication);
+        dispatcher = request.getRequestDispatcher(SHOW_ONE_PATH);
+        return dispatcher;
+    }
+
+    private RequestDispatcher getReferencesDispatcher(HttpServletRequest request, PublicationService service, Publication publication) {
+        RequestDispatcher dispatcher;
+        List<Publication> publications = service.getAll();
+        ReferencesService referencesService = new ReferencesService();
+        request.setAttribute("publications",
+                Objects.requireNonNull(
+                        referencesService
+                                .getPublicationsFromReferences(
+                                        publication.getReferences(),
+                                        publications)));
+        dispatcher = request.getRequestDispatcher(SHOW_ALL_PATH);
+        return dispatcher;
+    }
+
+    private RequestDispatcher getAllPublicationsDispatcher(HttpServletRequest request, PublicationService service) {
+        RequestDispatcher dispatcher;
+        FilteringService filteringService = new FilteringService();
+        List<Publication> publications = service.getAll();
+        String filtered = request.getParameter("filtered");
+        if (filtered != null && filtered.equals("true")) {
+            String doi = request.getParameter("doi");
+            String keyWords = request.getParameter("key_words");
+            if (doi != null && keyWords != null) {
+                publications = filteringService.filter(publications, doi, keyWords);
+            }
+        }
+        String sorted = request.getParameter("sorted");
+        if (sorted != null && sorted.equals("true")) {
+            publications = filteringService.sortByRelevance(publications);
+        }
+        request.setAttribute("publications", publications);
+        dispatcher = request.getRequestDispatcher(SHOW_ALL_PATH);
+        return dispatcher;
     }
 
     @Override
     protected void doGet(
             HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
         processRequest(request, response);
     }
 
